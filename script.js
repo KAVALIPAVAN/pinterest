67947d6774e15268c15d1f4249ad42923c58e114
KAVALI PAVAN <kavalipavan22@gmail.com>
2024-12-29 00:15:40 +0530

    first commit

---
+++ script.js
@@ -0,0 +1,39 @@
+// // "use strict";
+// function test() {
+//   console.log(this); // `this` is `undefined`
+// }
+// test();
+
+// function myFunction(a,b) { 
+//     "use strict";
+//     a = 20;  
+//     b=8;
+//     return args[0],args[1]; 
+//   }
+//   myFunction(2,5);
+  
+
+// function myFunction(a, b) { 
+//     // "use strict";
+//     a = 20;  
+//     b = 8;
+//     return arguments[0], arguments[1]; 
+// }
+// console.log(myFunction(2, 5));
+
+// const myFunction = (...args) => {
+//     'use strict';
+//     console.log(args[0]); // 10
+//     console.log(args[1]); // 20
+//   };
+//   myFunction(10, 20);
+  
+//   const arr=[12,25,6,78];
+//   const newArr=arr.map((n)=>n>12);
+//   console.log(newArr);
+
+
+// css elaborators
+// css pseudoclasses
+// flex and grid and difference  and 2d or 3d for both
+
